6a363a3b35d9d5c9ecb9a63682de1955b13c633f
Jack <chaster_killer@hotmail.fr>
2018-02-11 12:58:29 +0100

    Routes for the default gallery only

--- src/controllers.php
+++ src/controllers.php
@@ -76,126 +76,14 @@ $app->error(function (\Exception $e, $code) use ($app) {
 //
 // Controller: Home page.
 //
+/*
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig', array());
 })
 ->bind('homepage')
 ;
+*/
 
-//
-// Controller: Connect to a Google Drive account (to set up a gallery)
-//
-$app->get('/setup', function() use ($app) {
-    return $app['twig']->render('setup.twig', array('auth_url' => $app['authenticator']->getAuthUrl()));
-})
-->bind('setup')
-;
-
-//
-// Controller: Handle OAuth redirects from Google to authorize a gallery.
-//
-$app->get('/oauth', function(Application $app, Request $request) {
-    if ($error_code = $request->query->get('error')) {
-        if ($error_code == 'access_denied') { // The user refused to grant access.
-            $error = 'The connection attempt was canceled.';
-        } else {
-            $error = 'Connection failed with the following error: "' . $error_code . '"';
-        }
-        $app['session']->getFlashBag()->add('error', $error);
-
-        return $app->redirect($app['url_generator']->generate('setup'));
-    }
-
-    $auth_result = $app['authenticator']->authorizeGallery($request->query->get('code'));
-    if ($auth_result['success']) {
-        $app['session']->getFlashBag()->add('just-connected', true);
-        // $app['session']->getFlashBag()->add('success', 'Successfully connected to your Google Drive account.');
-
-        return $app->redirect($app['url_generator']->generate('settings'));
-    } else {
-        $app['session']->getFlashBag()->add('error', $auth_result['error']);
-
-        return $app->redirect($app['url_generator']->generate('setup'));
-    }
-});
-
-//
-// Controller: Deactivate gallery
-//
-$app->get('/deactivate', function(Application $app) {
-    if (!$app['user']) {
-        $app['session']->getFlashBag()->add('error', 'Please log in to find your gallery.');
-
-        return $app->redirect($app['url_generator']->generate('login'));
-    }
-
-    $galleryInfo = $app['gallery.info.mapper']->findByGoogleUserId($app['user']->getCustomField('googleUserId'));
-    if (!$galleryInfo) {
-        $app['session']->getFlashBag()->add('error', 'You haven\'t set up a gallery for this Google account yet.');
-
-        return $app->redirect($app['url_generator']->generate('setup'));
-    }
-
-    if ($app['gallery']->deactivateGallery($galleryInfo)) {
-        $app['session']->getFlashBag()->add('success', 'Disconnected from your Google Drive account and deactivated your gallery.');
-
-        return $app->redirect($app['url_generator']->generate('setup'));
-    } else {
-        $app['session']->getFlashBag()->add('error', 'Error disconnecting from your Google Drive account.');
-
-        return $app->redirect($app['url_generator']->generate('settings'));
-    }
-
-
-})
-->bind('deactivate');
-
-//
-// Controller: Manage settings for a gallery.
-//
-$app->match('/settings', function(Application $app, Request $request) {
-    if (!$app['user']) {
-        $app['session']->getFlashBag()->add('error', 'You must sign in to edit your settings.');
-        return $app->redirect($app['url_generator']->generate('login'));
-    }
-
-    /** @var GalleryInfo $galleryInfo */
-    $galleryInfo = $app['gallery.info.mapper']->findByGoogleUserId($app['user']->getCustomField('googleUserId'));
-    if (!$galleryInfo) {
-        $app['session']->getFlashBag()->add('error', 'You don\'t have a gallery set up yet. You can set one up below.');
-        return $app->redirect($app['url_generator']->generate('setup'));
-    }
-    if (!$galleryInfo->getIsActive()) {
-        $app['session']->getFlashBag()->add('error', 'Your gallery was deactivated. You can reactivate it by connecting to your Google Drive below.');
-        return $app->redirect($app['url_generator']->generate('setup'));
-    }
-
-    // Process form submissions.
-    if ($request->isMethod('POST')) {
-        $galleryInfo->setSlug($request->request->get('slug'));
-        $galleryInfo->setGalleryName($request->request->get('galleryName'));
-
-        $errors = $app['gallery']->validateGalleryInfo($galleryInfo);
-        if (count($errors) > 0) {
-            foreach ($errors as $error) {
-                $app['session']->getFlashBag()->add('error', $error);
-            }
-        } else {
-            // Save the gallery info
-            $app['gallery.info.mapper']->save($galleryInfo);
-            $app['session']->getFlashBag()->add('success', 'Saved gallery info.');
-
-            return $app->redirect($app['url_generator']->generate('my-gallery'));
-        }
-    }
-
-    return $app['twig']->render('edit.twig', array(
-        'galleryInfo' => $galleryInfo,
-        'galleryUrl' => 'http://drivegal.com/' . $galleryInfo->getSlug(),
-    ));
-})
-->bind('settings')
-;
 
 function renderTopLevelAlbum (Application $app, GalleryInfo $galleryInfo) {
     $albumContents = $app['gallery']->getAlbumContents($galleryInfo, '');
@@ -205,8 +93,8 @@ function renderTopLevelAlbum (Application $app, GalleryInfo $galleryInfo) {
         'subAlbums' => $albumContents->getSubAlbums(),
         'files' => '',
         'breadcrumbs' => $albumContents->getBreadcrumbs(),
-        'albumUrl' => $app['url_generator']->generate('album-list', array('gallery_slug' => $galleryInfo->getSlug())),
-        'streamUrl' => $app['url_generator']->generate('gallery', array('gallery_slug' => $galleryInfo->getSlug())),
+        'albumUrl' => $app['url_generator']->generate('default-album-list', array()),
+        'streamUrl' => $app['url_generator']->generate('defaultgallery', array()),
         'nextUrl' => '',
         'prevUrl' => '',
         'showLightboxPhoto' => '',
@@ -218,14 +106,6 @@ $app->get('/maingallery/album/', function(Application $app) {
   return renderTopLevelAlbum($app, getDefaultGallery($app));
 })
 ->bind('default-album-list')
-;
-//
-// Controller: View the top-level album list.
-//
-$app->get('/{gallery_slug}/album/', 'renderTopLevelAlbum' )
-->assert('gallery_slug', '^[^_][^/]+') // slug can't start with an underscore or contain a slash (we have to specify that manually since we override the default regex).
-->convert('galleryInfo', $galleryProvider)
-->bind('album-list')
 ;
 
 //
@@ -246,8 +126,8 @@ function renderAlbum(Application $app, GalleryInfo $galleryInfo, $album_path) {
         'files' => $albumContents->getFiles(),
         'subAlbums' => $albumContents->getSubAlbums(),
         'breadcrumbs' => $albumContents->getBreadcrumbs(),
-        'albumUrl' => $app['url_generator']->generate('album-list', array('gallery_slug' => $galleryInfo->getSlug())),
-        'streamUrl' => $app['url_generator']->generate('gallery', array('gallery_slug' => $galleryInfo->getSlug())),
+        'albumUrl' => $app['url_generator']->generate('default-album-list', array()),
+        'streamUrl' => $app['url_generator']->generate('defaultgallery', array()),
         'showLightboxPhoto' => '',
         'gallerySlug' => $galleryInfo->getSlug(),
     ));
@@ -262,13 +142,6 @@ $app->get('/maingallery/album/{album_path}/',
 ->value('album_path', '')
 ->bind('defaultalbum');
 
-$app->get('/{gallery_slug}/album/{album_path}/', 'renderAlbum')
-// slug can't start with an underscore or contain a slash (we have to specify that manually since we override the default regex).
-->assert('album_path', '.*') // album path *can* contain slashes.
-->convert('galleryInfo', $galleryProvider)
-->value('album_path', '')
-->bind('album')
-;
 
 //
 // Middleware: Determine the current user.
@@ -283,51 +156,6 @@ $app->before(function (Symfony\Component\HttpFoundation\Request $request) use ($
     }
 });
 
-//
-// Controller: Login
-//
-$app->get('/login', function () use ($app) {
-    $services = array_keys($app['oauth.services']);
-
-    return $app['twig']->render('login.twig', array(
-        'login_paths' => array_map(function ($service) use ($app) {
-            return $app['url_generator']->generate('_auth_service', array(
-                'service' => $service,
-                '_csrf_token' => $app['form.csrf_provider']->generateCsrfToken('oauth')
-            ));
-        }, array_combine($services, $services)),
-        'logout_path' => $app['url_generator']->generate('logout', array(
-                '_csrf_token' => $app['form.csrf_provider']->generateCsrfToken('logout')
-            ))
-    ));
-})
-->bind('login');
-
-//
-// Controller: Logout
-//
-$app->match('/logout', function () {})->bind('logout');
-
-//
-// Controller: View my gallery
-//
-$app->get('/my-gallery', function(Application $app) {
-    if (!$app['user']) {
-        $app['session']->getFlashBag()->add('error', 'Please log in to find your gallery.');
-
-        return $app->redirect($app['url_generator']->generate('login'));
-    }
-
-    $galleryInfo = $app['gallery.info.mapper']->findByGoogleUserId($app['user']->getCustomField('googleUserId'));
-    if (!$galleryInfo) {
-        $app['session']->getFlashBag()->add('error', 'You haven\'t set up a gallery for this Google account yet.');
-
-        return $app->redirect($app['url_generator']->generate('setup'));
-    }
-
-    return $app->redirect($app['url_generator']->generate('gallery', array('gallery_slug' => $galleryInfo->getSlug())));
-})
-->bind('my-gallery');
 
 function renderGalleryPage(Application $app, Request $request, GalleryInfo $galleryInfo, $pagestr) {
     $pg = substr($pagestr, 4); // Strip out the leading "page" string.
@@ -338,7 +166,7 @@ function renderGalleryPage(Application $app, Request $request, GalleryInfo $gall
         $photoStream->count(),
         $photoStream->getPerPage(),
         $pg,
-        $app['url_generator']->generate('gallery', array('gallery_slug' => $galleryInfo->getSlug())) . '/page(:num)'
+        $app['url_generator']->generate('defaultgallery', array()) . '/page(:num)'
     );
 
     return $app['twig']->render('stream.twig', array(
@@ -358,14 +186,4 @@ $app->get('/maingallery/{pagestr}', function(Application $app, Request $request,
     ->assert('pagestr', '^page\d+')
     ->value('pagestr', 'page1')
     ->bind('defaultgallery');
-
-//
-// Controller: View a gallery's photo stream
-//
-$app->get('/{gallery_slug}/{pagestr}', 'renderGalleryPage')
-    ->assert('gallery_slug', '^[^_][^/]+') // slug can't start with an underscore or contain a slash.
-    ->assert('pagestr', '^page\d+')
-    ->convert('galleryInfo', $galleryProvider)
-    ->value('pagestr', 'page1')
-    ->bind('gallery');
 
